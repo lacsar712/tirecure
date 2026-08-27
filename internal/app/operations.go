14fa9684a1d3399188d866bd25349dc2b76a40d4
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-27 11:11:37 +0800

    init: tirecure

---
+++ internal/app/operations.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"context"
+	"time"
+
+	"github.com/lacsar712/tirecure/internal/model"
+)
+
+func (a *App) ValidateMoldDrift(ctx context.Context, moistPct float64) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+	limit := a.cfg.TargetMoistPct + a.cfg.MaxGradientDeltaPct
+	if moistPct <= limit {
+		return nil
+	}
+	return model.DriftChain(moistPct)
+}
+
+func (a *App) ConfirmBladderHold(ctx context.Context, anchor time.Time) error {
+	if a.avgWindow == nil {
+		return model.Wrap("app", "window", model.ErrBladderHold)
+	}
+	if err := a.avgWindow.Require(anchor); err != nil {
+		return model.HoldChain(err)
+	}
+	if a.scheduler != nil && a.scheduler.VentStepsDone() == 0 {
+		return model.Wrap("app", "schedule", model.ErrScheduleEmpty)
+	}
+	return nil
+}
